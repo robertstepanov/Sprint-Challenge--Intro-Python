402d7e4dfef0617398ccd484ad1e49f13e4401c8
Robert <robertdstepanov@gmail.com>
2020-05-08 12:35:51 -0400

    Comp, oop1 and oop2 passing

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -3,13 +3,16 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
+
 class GroundVehicle():
-    def __init__(self, num_wheels):
+
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
     # TODO
 
-
+    def drive(self):
+        return 'vroooom'
 # Subclass Motorcycle from GroundVehicle.
 #
 # Make it so when you instantiate a Motorcycle, it automatically sets the number
@@ -17,7 +20,15 @@ class GroundVehicle():
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__(num_wheels=2)
+
+    def drive(self):
+        return 'BRAAAP!!'
 # TODO
+
 
 vehicles = [
     GroundVehicle(),
@@ -30,3 +41,6 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+
+for i in vehicles:
+    print(i.drive())
